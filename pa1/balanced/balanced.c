8c4a1fa8d979b8cf31d7b0db62f91e25d9b1527b
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-01-23 19:26:30 -0500

    balanced complete

--- pa1/balanced/balanced.c
+++ pa1/balanced/balanced.c
@@ -18,10 +18,78 @@ int main(int argc, char* argv[])
     }
 
     bool balanced = true;
-
     char buff;
+    struct element* top = malloc(sizeof(struct element)); //top is the pointer pointing to the head of the stack
+    top->next = NULL; 
     while ( fscanf(fp, "%c", &buff)==1 ) {
+
+
+    //push function of the stack
+        if(buff == '<')
+        {
+            struct element* current = malloc(sizeof(struct element));
+            current->close = '>';
+            current->next = top->next;
+            top->next = current;
+        }
+        else if (buff == '(')
+        {
+            struct element* current = malloc(sizeof(struct element));
+            current->close = ')';
+            current->next = top->next;
+            top->next = current;
+        }
+        else if (buff == '[')
+        {
+            struct element* current = malloc(sizeof(struct element));
+            current->close = ']';
+            current->next = top->next;
+            top->next = current;
+        }
+        else if (buff == '{')
+        {
+            struct element* current = malloc(sizeof(struct element));
+            current->close = '}';
+            current->next = top->next;
+            top->next = current;
+        }
+    
+    //pop function of the stack
+
+        else if(buff == '>' || buff == '}' || buff == ']' || buff == ')')
+        {
+            struct element* temp = top;
+            struct element* f;
+            while(temp->next != NULL && buff != temp->next->close)
+            {
+                temp = temp->next;
+            }
+            if(temp->next == NULL)
+            {
+                balanced = false;
+                break;
+            }
+            f = temp->next;
+            temp->next = temp->next->next;
+            free(f);
+        }
+       
     }
+
+
+     struct element* temp;
+     while(top != NULL)
+     {
+         temp = top;
+         top = top->next;
+         if(top != NULL)
+         {
+            balanced = false;
+            //printf("%c ", top->close);
+         }
+         free(temp);
+     }
+     //printf ("\n");
 
     printf ( balanced ? "yes" : "no" );
     fclose(fp);
