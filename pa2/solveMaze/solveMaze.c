e7df4cc7858508f39d3c9ec64db63d9e83a9ca39
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-02-26 15:43:28 -0500

    finish solveMaze

--- pa2/solveMaze/solveMaze.c
+++ pa2/solveMaze/solveMaze.c
@@ -17,23 +17,55 @@ typedef struct Queue Queue;
 // Append a new QueueNode to the back of the Queue
 struct Queue enqueue ( struct Queue queue, graphNode_t graphNode ) {
     /* ... */
+    QueueNode* newnode = malloc(sizeof(QueueNode));
+    newnode->graphNode = graphNode;
+    newnode->next = NULL;
+    if(queue.back)
+    {
+        queue.back->next = newnode;
+        queue.back = newnode;
+    }
+    else
+    {
+        queue.back = newnode;
+        queue.front = newnode;
+    }
+
     return queue;
 }
 
 // Remove a QueueNode from the front of the Queue
 graphNode_t dequeue ( Queue* queue ) {
     /* ... */
+    graphNode_t node = queue->front->graphNode;
+    QueueNode* temp = queue->front;
+    if(queue->front == queue->back)
+    {
+        queue->back = NULL;
+    }
+    queue->front = temp->next;
+    free(temp);
+    return node;
 }
 
 int main ( int argc, char* argv[] ) {
 
     // First, read the query file to get the source and target nodes in the maze
     /* ... */
-
+    FILE *fp = fopen(argv[2],"r");
+    if(!fp)
+    {
+        perror("fopen failed");
+        return 1;
+    }
+    graphNode_t current, target, source;
+    fscanf(fp, "%ld", &source);
+    fscanf(fp, "%ld", &target);
+    current = source; 
     // READ INPUT FILE TO CREATE GRAPH ADJACENCY LIST
     AdjacencyListNode* adjacencyList = NULL;
     /* ... */
-
+    size_t graphNodeCount = adjMatrixToList(argv[1], &adjacencyList);
     // USE A QUEUE TO PERFORM BFS
     Queue queue = { .front=NULL, .back=NULL };
 
@@ -42,7 +74,6 @@ int main ( int argc, char* argv[] ) {
     for (size_t i=0; i<graphNodeCount; i++) {
         parents[i] = -1; // -1 indicates that a nodes is not yet visited
     }
-
     /* ... */
 
     while ( current != target ) {
@@ -50,15 +81,47 @@ int main ( int argc, char* argv[] ) {
         // so long as we haven't found the target node yet, iterate through the adjacency list
         // add each neighbor that has not been visited yet (has no parents) to the queue of nodes to visit
         /* ... */
-
+        //AdjacencyListNode* neighbor = (adjacencyList[current]).next;
+        //while(adjacencyList[current])
+        AdjacencyListNode* neighbor = adjacencyList[current].next;
+        while(neighbor)
+        {
+            if(parents[neighbor->graphNode] == -1)
+            {
+                queue = enqueue(queue, neighbor->graphNode);
+                parents[neighbor->graphNode] = current;
+            }
+            neighbor = neighbor->next;
+        }
         // Visit the next node at the front of the queue of nodes to visit
         /* ... */
+        if(queue.front)
+        {
+            //printf("queue front is %zu\n", queue.front->graphNode);
+            current = dequeue(&queue);
+        }
+        else
+        {
+            break;
+        }
     }
 
     // Now that we've found the target graph node, use the parent array to print maze solution
     // Print the sequence of edges that takes us from the source to the target node
     /* ... */
-
+    if(parents[target] == -1)
+    {
+        printf("There is no path from %zu to %zu\n", source, target);
+    }
+    else 
+    {
+        while(parents[current] != source)
+        {
+            printf("%zu %zu\n", current, parents[current]);
+            current = parents[current];
+        }
+        printf("%zu %zu\n", current, parents[current]);
+    }
     // free any queued graph nodes that we never visited because we already solved the maze
     while ( queue.front ) {
         dequeue(&queue);
