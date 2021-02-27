8a2e6d1e8350a213ba0f86b35baa795c863ea3d7
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-02-27 16:25:24 -0500

    mst finished

--- pa2/mst/mst.c
+++ pa2/mst/mst.c
@@ -5,8 +5,8 @@
 int main ( int argc, char* argv[] ) {
 
     // READ INPUT FILE TO CREATE GRAPH ADJACENCY LIST
-    AdjacencyListNode* adjacencyList;
-    /* ... */
+    AdjacencyListNode* adjacencyList = NULL;
+    size_t graphNodeCount = adjMatrixToList(argv[1], &adjacencyList);
 
     // An array that keeps track of who is the parent node of each graph node we visit
     // In Prim's algorithm, this parents array keeps track of what is the edge that connects a node to the MST.
@@ -17,7 +17,8 @@ int main ( int argc, char* argv[] ) {
 
     graphNode_t root = rand()%graphNodeCount;
     parents[root] = root;
-
+    //printf("root is %zu\n", root);
+    int flag = 0;
     // Prim's algorithm:
     // A greedy algorithm that builds the minimum spanning tree.
     // For a graph with N nodes, the minimum spanning tree will have N-1 edges spanning all nodes.
@@ -32,13 +33,42 @@ int main ( int argc, char* argv[] ) {
         for (graphNode_t source=0; source<graphNodeCount; source++) {
             if (parents[source]!=-1) { // if already visited
                 /* ... */
+                AdjacencyListNode* current = adjacencyList[source].next;
+                while(current)
+                {
+                    bool no_dup = (parents[source] != current->graphNode) && (parents[current->graphNode] == -1);
+                    if(current->weight < minWeight && no_dup)
+                    {
+                        
+                        minWeight = current->weight;
+                        minDest = current->graphNode;
+                        minSource = adjacencyList[source].graphNode;
+                        //printf("minDest is %zu, and minSource is %zu\n", minDest, minSource);
+                    }
+                    current = current->next;
+                }
             }
+        }
+        if(minDest == -1 && minSource == -1)
+        {
+            flag++;
         }
         parents[minDest]=minSource; // we found the minimum weight
     }
 
     // Using the fully populated parents array, print the edges in the MST.
     /* ... */
+    if(flag > 1)
+    {
+        perror(" no minimum spanning tree in the graph\n");
+    }
+    for(int i=0; i<graphNodeCount; i++)
+    {
+        if(parents[i] != -1 && parents[i] != i)
+        {
+            printf("%d %zu\n", i, parents[i]);
+        }
+    }
 
     free (parents);
     freeAdjList ( graphNodeCount, adjacencyList );
