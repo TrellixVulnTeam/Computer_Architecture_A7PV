e2ccfacbb827aafd713f73c06e76c0a9206c2df6
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-03-18 18:24:12 -0400

    finish toHex

--- pa3/toHex/toHex.c
+++ pa3/toHex/toHex.c
@@ -15,15 +15,32 @@ int main(int argc, char *argv[]) {
     fscanf(fp, "%hd", &input); // note how the 'h' modifier shortens the %d to half size
 
     // print bits; you will see this kind of for loop often in this assignment
-    for ( int digit=15; 0<=digit; digit-- ) {
-        bool char_val = 0b1 & input>>digit; // shift and mask
-        printf("%d",char_val);
-    }
-    printf("\n");
+    // for ( int digit=15; 0<=digit; digit-- ) {
+    //     bool char_val = 0b1 & input>>digit; // shift and mask
+    //     printf("%d",char_val);
+    // }
+    // printf("\n");
 
     // useful hints for printing uppercase hexadecimal
-    printf("The character value of '0' = %d\n",'0');
-    printf("The character value of 'A' = %d\n",'A');
+    //printf("The character value of '0' = %c\n",65);
+    // printf("The character value of 'A' = %d\n",'A');
+    int sum = 0;
+    for( int digit=15; digit>=0; digit--)
+    {
+        int char_val = 0b1 & input>>digit;
+        sum += char_val<<(digit%4);
+        if(digit%4 == 0)
+        {
+            if(sum >= 10)
+            {
+                sum += 7;
+            }
+            printf("%c", sum+48);
+            sum = 0;
+        }
+
+    }
+    fclose(fp);
 
     return EXIT_SUCCESS;
 
