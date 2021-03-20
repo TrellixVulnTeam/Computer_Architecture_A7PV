dcf175c1edf74311df7cde46a8fe4dc0392bf9bc
Xiaowei Zhang <blackcatecho1998@gmail.com>
2021-03-20 16:24:15 -0400

    finish doubleToBin

--- pa3/doubleToBin/doubleToBin.c
+++ pa3/doubleToBin/doubleToBin.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[]) {
     // you are not allowed to print from this casted 'ref_bits' variable below
     // but, it is helpful for validating your solution
     unsigned long int ref_bits = *(unsigned long int*) &value;
+    //printf("%lu\n", ref_bits);
 
     // THE SIGN BIT
     bool sign = value<0.0;
@@ -35,15 +36,30 @@ int main(int argc, char *argv[]) {
     // THE EXP FIELD
     // find the exponent E such that the fraction will be a canonical fraction:
     // 1.0 <= fraction < 2.0
-    double fraction = sign ? -value : value;
+    //double fraction = sign ? -value : value;
 
     signed short trial_exp=(1<<(EXP_SZ-1))-1; // start by assuming largest possible exp (before infinity)
     // do trial division until the proper exponent E is found
     /* ... */
-
+    double quotient = 0;
+    while((quotient >=2 && trial_exp >=-1022)|| (quotient <1 && trial_exp >=-1022))
+    {
+        quotient = sign ? -(value / pow(2, trial_exp)) : (value / pow(2, trial_exp));
+        trial_exp--;
+    }
+    //printf("%d\n", trial_exp+1);
+    trial_exp++;
     unsigned short bias = (1<<(EXP_SZ-1))-1;
     signed short exp = trial_exp + bias;
-
+    if(trial_exp == -1022 && quotient <1)
+    {
+        exp = 0;
+    }
+    else
+    {
+        quotient -= 1;
+    }
+    
     for ( int exp_index=EXP_SZ-1; 0<=exp_index; exp_index-- ) {
         bool exp_bit = 1&exp>>exp_index;
         printf("%d",exp_bit);
@@ -58,20 +74,37 @@ int main(int argc, char *argv[]) {
     // prepare the canonical fraction such that:
     // 1.0 <= fraction < 2.0
     /* ... */
-
+    //printf("quotient is %f\n", quotient);
     bool frac_array[FRAC_SZ+1]; // one extra LSB bit for rounding
     for ( int frac_index=FRAC_SZ; 0<=frac_index; frac_index-- ) {
         frac_array[frac_index] = false; // frac set to zero to enable partial credit
         /* ... */
+        quotient = quotient*2;
+        if(quotient >= 1)
+        {
+            frac_array[frac_index] = true;
+            quotient--;
+        }
+        //printf("%d", frac_array[frac_index]);
+        
     }
-
+    //putchar('\n');
     // rounding
     /* ... */
+    bool carry = frac_array[0];
+    int frac_index = 1;
+    while(frac_array[frac_index] && carry)
+    {
+        frac_array[frac_index] = false;
+        frac_index++;
+    }
 
     for ( int frac_index=FRAC_SZ-1; 0<=frac_index; frac_index-- ) {
         bool frac_bit = frac_array[frac_index+1]; // skipping the extra LSB bit for rounding
         printf("%d", frac_bit);
-        // assert (frac_bit == (1&ref_bits>>frac_index)); // validate your result against the reference
+        assert (frac_bit == (1&ref_bits>>frac_index)); // validate your result against the reference
     }
+    fclose(fp);
+    return EXIT_SUCCESS;
 
 }
